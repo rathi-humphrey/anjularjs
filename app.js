a850ca8823c4a893c58c856a0175c65f7d7c4e01
rathi-humphrey <rathi.humphrey@gmail.com>
2018-02-13 01:51:43 -0800

    Service and controller

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var myapp = angular.module('myapp',[]);
 myapp.controller('MyCtrl',['$scope','AppService',function($scope,AppService){
     AppService.getStudents().then(function(result){
-        $scope.students=result.data;
-    });
+        $scope.students=result;
+        });
 $scope.students = AppService.getStudents();
 $scope.onViewBtnClick = function(student){
     $scope.selectedStudent = student;
--- app.service.js
+++ app.service.js
@@ -1,5 +1,5 @@
 var myapp = angular.module('myapp');
-myapp.service('AppService',['$http',function($http){
+myapp.service('AppService',['$http','$q',function($http,$q){
 var students = [
     {
     name : 'Joshua',
@@ -12,7 +12,15 @@ var students = [
 
 ];
 this.getStudents = function(){
- return $http.get('/students.json');
+ var defer = $q.defer();
+ $http.get('/students.json').then(function(result){
+     var students=result.data.map(function(item){
+        item.senior = item.age <10 ?"junior":"senior";
+        return item;
+     });
+        defer.resolve(students);
+ });
+ return defer.promise;
     
 }
 }]);
